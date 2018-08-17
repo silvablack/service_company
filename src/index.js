a27148f5a93084317d8d52e5d1834eae3b0d3c41
Paulo Silva <paulosilvadev3@gmail.com>
2018-08-16 23:03:49 -0300

    fix routes

--- src/index.js
+++ src/index.js
@@ -7,34 +7,11 @@
 const app = require('./app');
 
 /**
- * @requires body parser to Response
- */
-const bodyParser = require("body-parser");
-
-/**
- * @requires config routes to site index and company
- */
-const index = require("./routes/index");
-const companyRoute = require("./routes/companyRoute");
-
-/**
  * @description Define host and port to server listening
  */
 const _PORT = process.env.PORT_SERVICE_COMPANY;
 const _HOST = process.env.URL_SERVICE_COMPANY;
 
-
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-app.use(bodyParser.json());
-
-/**
- * @description Set routes config on server
- */
-app.use('/', index);
-app.use('/company', companyRoute);
 /**
  * @description init listening
  */
